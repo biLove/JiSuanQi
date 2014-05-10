2651544588575b02709c7932679e7d975dd43b68
biLove <beyourself1997@gmail.com>
2014-05-10 21:45:41 +0800

    deal with some operator
    符号键连续使用

--- src/com/baoli/jisuanqi/CalculatorCore.java
+++ src/com/baoli/jisuanqi/CalculatorCore.java
@@ -1,28 +1,31 @@
 package com.baoli.jisuanqi;
 
-
 public class CalculatorCore {
-	
+
 	// private EditText screenText;
 	public double mScreenNum = 0;
 	public String mPreOpn = "=";
 	public int mOpnNum = 0;
 	public double mFirstNum = 0;
-	
-	
-	public CalculatorCore(){
+	public boolean mPreIsNum = true;
+
+	public CalculatorCore() {
 	}
-	
-	public String clickClear(){
+
+	public String clickClear() {
 		mScreenNum = 0;
+		mFirstNum = 0;
+		mPreOpn = "=";
+		mOpnNum = 0;
 		return "0";
 	}
-	
-	public String clickNum(String num){
+
+	public String clickNum(String num) {
 
 		double i = Double.valueOf(num).doubleValue();
 		mScreenNum = mScreenNum * 10 + i;
 		String result = String.valueOf(mScreenNum);
+		mPreIsNum = true;
 		return result;
 	}
 
@@ -31,7 +34,6 @@ public class CalculatorCore {
 		try {
 			if (opt.equals("=") && mOpnNum == 0) {
 				mFirstNum = mScreenNum;
-				mScreenNum = 0;
 				result = String.valueOf(mFirstNum);
 			}
 
@@ -48,23 +50,25 @@ public class CalculatorCore {
 			else if (mOpnNum == 1
 					&& (opt.equals("=") || opt.equals("+") || opt.equals("-")
 							|| opt.equals("*") || opt.equals("/"))) {
+				if (mPreIsNum) {
 
-				if (mPreOpn.equals("+")) {
-					mFirstNum += mScreenNum;
-				} else if (mPreOpn.equals("-")) {
-					mFirstNum -= mScreenNum;
-				} else if (mPreOpn.equals("*")) {
-					mFirstNum *= mScreenNum;
-				} else if (mPreOpn.equals("/")) {
-					if (mScreenNum != 0)
-						mFirstNum /= mScreenNum;
-					else
-						throw new ArithmeticException();
+					if (mPreOpn.equals("+")) {
+						mFirstNum += mScreenNum;
+					} else if (mPreOpn.equals("-")) {
+						mFirstNum -= mScreenNum;
+					} else if (mPreOpn.equals("*")) {
+						mFirstNum *= mScreenNum;
+					} else if (mPreOpn.equals("/")) {
+						if (mScreenNum != 0)
+							mFirstNum /= mScreenNum;
+						else
+							throw new ArithmeticException();
+					}
 				}
 				result = String.valueOf(mFirstNum);
 				if (opt.equals("=")) {
 					mOpnNum = 0;
-					mScreenNum = 0;
+					mScreenNum = mFirstNum;
 				} else if (opt.equals("+") || opt.equals("-")
 						|| opt.equals("*") || opt.equals("/")) {
 					// screenNum = 0;
@@ -74,12 +78,13 @@ public class CalculatorCore {
 				}
 			}
 		} catch (NumberFormatException e) {
-			result="Number Format ERROR!";
+			result = "Number Format ERROR!";
 		} catch (ArithmeticException e) {
-			result="Div Number CAN NOT a ZERO!";
+			result = "Div Number CAN NOT a ZERO!";
 			mPreOpn = "=";
 		} finally {
 		}
+		mPreIsNum = false;
 		return result;
 	}
 
